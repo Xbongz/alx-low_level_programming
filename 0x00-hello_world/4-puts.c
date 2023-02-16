217659cc09b6a985b0736a69b1718c463870222a
Bonginkosi <gbongig@gmail.com>
2023-02-16 04:06:20 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A hello puts program thats print
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
