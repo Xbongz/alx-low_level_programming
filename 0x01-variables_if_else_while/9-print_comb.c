cd989943ecaf716e60ae20f3ab0c88c6b50cc859
Bonginkosi <gbongig@gmail.com>
2023-02-17 07:32:51 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible single digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(",);
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
