3503b6bbd72c25a7b753e34dda3dab3fa5639da7
Bonginkosi <gbongig@gmail.com>
2023-02-17 07:22:00 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints 16 base lowercase numbers,
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
