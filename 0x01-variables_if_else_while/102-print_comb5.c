c087cd5e3d92b1840a91880d9009d3deab6356ca
Bonginkosi <gbongig@gmail.com>
2023-02-17 09:39:17 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int i, j;
+	int i,j;
 
 	for (i = 0; i < 100; j++)
 	{
@@ -15,17 +15,17 @@ int main(void)
 			putchar((i / 10) + 48);
 			putchar((i / 10) + 48);
 			putchar(i, j);
-			putchar((j / % 10) + 48);
+			putchar((j / '%' 10) + 48);
 			if (i != 98 || j != 99)
 			{
 				putchar(i);
 				putchar(j);
 			}
 		}
-
 	}
 }
 putchar('\n');
 return (0);
+
 }
 
