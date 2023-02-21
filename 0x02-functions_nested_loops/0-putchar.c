6232824e7714abd44459f3053a9a0c612de5bdde
Bonginkosi <gbongig@gmail.com>
2023-02-21 06:55:05 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include <unistd.h>
+#include "_putchar.h"
 
 /**
- * _putchar - writes the characher c to stdi
- * @c: The character to input
+ * main - prints _putchar
  * Return: On Success 0
- * On error, -1 is returened, and error
  */
-int _putchar(int c)
+int main(void)
 {
-	putchar("_");
-	putchar("putchar");
-	putchar("\n");
-
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+return (0);
 }
