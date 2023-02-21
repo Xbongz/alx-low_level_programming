c9b6af7be51da5e40a6ccbb63903b24e9772f2c5
Bonginkosi <gbongig@gmail.com>
2023-02-21 02:41:34 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints _putchar,
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void);
+{
+	printf("_putchar"\n");
+	return (0);
+}
