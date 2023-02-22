8efcfd450bb9c6fbc467f9d6d47f19cd80434bab
Bonginkosi <gbongig@gmail.com>
2023-02-22 05:30:06 -0800

    0. Multiple mains

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,1 +1,16 @@
-positive_or_negative()
+#include "main.h"
+
+/**
+ * main - tests function that prints integer is positive or negative
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
