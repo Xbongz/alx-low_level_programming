ea5ed8d8cf2634430d0a2ecd032cb5ad7f3cd73f
Bonginkosi <gbongig@gmail.com>
2023-02-23 10:34:00 -0800

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
-int_putchar(char c);
-int _isupper(int c);
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
+int _isupper(int i);
 int _isdigit(int c);
-int mul(int a, int b);
+int mul(int x, int y);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
+void print_line(int j);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
 #endif
