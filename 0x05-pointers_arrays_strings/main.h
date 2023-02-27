2b007dda392d04fb4526c3015356d3003d41e38b
Bonginkosi <gbongig@gmail.com>
2023-02-27 05:43:14 -0800

    doing

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,3 +12,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
