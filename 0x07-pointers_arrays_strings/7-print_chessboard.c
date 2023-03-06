3f8495739fd39bcdab7ab97938ca1199c097fb99
Bonginkosi <gbongig@gmail.com>
2023-03-06 13:36:22 -0800

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,6 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
  * print_chessboard - Entry point
  * @a: array
@@ -7,6 +9,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+	
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
