910c106d99295ebbfcfcf874efb9f5ae1881cc31
Bonginkosi <gbongig@gmail.com>
2023-03-08 10:33:22 -0800

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: The string to be printed.
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ *
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
