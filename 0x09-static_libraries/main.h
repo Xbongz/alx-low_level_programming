f0e826f4aaf162c34d6fdca3d02e867fe17498e5
Bonginkosi <gbongig@gmail.com>
2023-03-15 09:47:23 -0700

    main

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*_HOLBERTON_H_*/
+#endif /* _HOLBERTON_H_ */
