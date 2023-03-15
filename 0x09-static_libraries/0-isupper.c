373549f44870f87637867ef25a9b54a5ae2b00a5
Bonginkosi <gbongig@gmail.com>
2023-03-15 03:49:32 -0700

    strlen

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -9,6 +9,5 @@ int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	else
 	return (0);
 }
--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -4,7 +4,7 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void	
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
@@ -23,8 +23,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
-
+	}
 
 	dest[i] = '\0';
 	return (dest);
