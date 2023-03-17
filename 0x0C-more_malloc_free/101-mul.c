d2d973d626d737eaa23987948abe43d86b2104e8
Bonginkosi <gbongig@gmail.com>
2023-03-16 18:39:41 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,6 +13,7 @@
 int is_digit(char *s)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -32,6 +33,7 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -61,6 +63,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
