c80450e269f59f2b6c62c2d88458adcb6b0ac72d
Bonginkosi <gbongig@gmail.com>
2023-03-16 18:24:10 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,6 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *ptr1;
 	char *old_ptr;
 	unsigned int i;
+
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr)
