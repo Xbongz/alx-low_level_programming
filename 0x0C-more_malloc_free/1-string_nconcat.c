1ab360a3bd4c8e3a78d0b7c2c5da5f1525a33de3
Bonginkosi <gbongig@gmail.com>
2023-03-16 18:10:34 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
