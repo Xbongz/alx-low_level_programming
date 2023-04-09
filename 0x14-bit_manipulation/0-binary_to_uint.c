e180d74e7d6819ebe98beaef862ad285cd71f0fa
Bonginkosi <gbongig@gmail.com>
2023-04-09 03:09:02 -0700

    convert binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
-/*
+/**
  * binary_to_uit - convert a binary to unsigned interger
  * @b: convert the binary string
  *
  * Return: unsigned intetger
  */
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 	int i = 0;
